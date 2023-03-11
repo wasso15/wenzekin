e3204c32cfbf327139ef33d9769ea953023b482e
Guylain wassos <wassoguylain15@gmail.com>
2023-03-12 00:12:03 +0100

    make footer responsive

--- components/Footer.js
+++ components/Footer.js
@@ -5,9 +5,10 @@ import Image from 'next/image'
 
 function Footer() {
   return (
-    <footer className=' h-[300px] bg-green-base flex flex-col items-center justify-center'>
-        <div className=' h-[80%]  w-[100%] mx-auto flex flex-row items-center  '>
-            <div className='w-[500px] h-[200px] mr-[10%] flex flex-col items-center justify-center'>
+    <footer className=' md:h-[300px] bg-green-base flex flex-col items-center justify-center'>
+        <div className=' h-[80%] w-[100%] mx-auto flex flex-col-reverse lg:flex-row items-center justify-evenly '>
+            
+            <div className='md:w-[300px] md:h-[200px]  flex flex-col items-center justify-center my-4 md:my-0 '>
                 <div className=' flex flex-row items-center'>
                     <a href='#'  className="w-[40px] h-[35px]  md:h-[50px] md:w-[60px] relative "> 
                        <Image src={ImagesAssets.logoWenzeKin} fill={true}  object-fit= "contain"  alt='Logo wenzeKin'/>
@@ -20,22 +21,23 @@ function Footer() {
                 <div  className=" text-white font-semibold "> AllFactoring Entreprise Ltd  </div>
 
             </div>
-            <div className=' flex flex-row w-[550px] justify-between items-center'>
+
+            <div className=' flex flex-col  md:flex-row md:w-[550px] justify-between md:items-center'>
              
-              <ul className=''>
+              <ul className=' mt-8 mb-4 md:my-0'>
                 <li className=' leading-3 font-bold text-white mb-3'> Entreprise </li>
                 <li> <a href='#' className='text-sm text-white'> A propos  </a> </li>
                 <li> <a href='#' className='text-sm text-white leading-3'> Nos Shops  </a> </li>
                 <li> <a href='#' className='text-sm text-white'> FAQ  </a> </li>
               </ul>
 
-              <ul className=''>
+              <ul className='my-4 md:my-0'>
                 <li className=' leading-3 font-bold text-white mb-3'>  Services   </li>
                 <li> <a href='#' className='text-sm text-white'> Livraison </a> </li>
                 <li> <a href='#' className='text-sm text-white leading-3'> Payement </a> </li>
                 <li> <a href='#' className='text-sm text-white'> Contacts  </a> </li>
               </ul>
-              <ul className=''>
+              <ul className='my-4 md:my-0'>
                 <li className=' leading-3 font-bold text-white mb-3'>  Suivez nous  </li>
                 <li> <a href='#' className='text-sm text-white'> Facebook  </a> </li>
                 <li> <a href='#' className='text-sm text-white leading-3'> Instagram  </a> </li>
