2b23a7720b561ac7bb72529a61c5f2496b0ee6cc
Guylain wasso <wassoguylain15@gmail.com>
2023-08-15 10:29:39 -0700

    add navbar and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  
   theme: {
     extend: {
+      colors: {
+        'green-base': '#128D02',
+        'orange-medium': '#F29305',
+        'silver': '#979797',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
