76fefde916d85f8fada7ec8748183e6b48740680
Guylain wasso <wassoguylain15@gmail.com>
2023-08-16 13:57:09 -0700

    add home page

--- app/components/BannerHero.js
+++ app/components/BannerHero.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Image from 'next/image'
-// import { ImagesAssets } from '@/public/images/imageAssets';
 import { urlFor } from '@/lib/client';
 
 
+
 function BannerHero({data}) {
-    const bannerImg= urlFor(data.image).url(); 
-    console.log(bannerImg)
+  console.log('bannier log', data[0])
+    const bannerImg= urlFor(data[0].image).url(); 
   return (
-    <div className='w-[95%]  my-5 md:my-10 mx-auto'>
+    <div className='w-[95%] my-5 md:my-10 mx-auto'>
             <img src={bannerImg} className='h-auto max-w-full md:w-full object-cover  rounded-b-xl' />
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -1,12 +1,54 @@
 import Image from 'next/image'
+import BannerHero from './components/BannerHero'
+import SliderCards from './components/SliderCards'
+import Title from './components/Title'
+import Product from './components/Product'
+import HowItWorks from './components/HowItWorks'
+import client from '@/lib/client'
+import { groq } from "next-sanity";
 
-export default function Home() {
+
+export default async function Home() {
+
+  async function getBanniere() {
+    return client.fetch(groq`*[_type=="Banner"]`);
+  }
+
+  async function getProducts() {
+    return client.fetch(groq`*[_type=="Products"]`);
+  }
+
+  const banner= await getBanniere()
+  const productData= await getProducts()
+
+  console.log(productData)
+
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div>
-        Hello a tous 
-      </div>
+      <header>
+        <BannerHero data={banner} />
+      </header>
+      <main className="">
+        <div className=" flex flex-row items-center w-[90%] my-10 mx-auto">
+          
+          <SliderCards productData={productData}>Le plus populaire</SliderCards>
+          
+        </div>
 
+        <div className=" w-[80%]  mx-auto py-5">
+            <Title>Nos produits</Title>
+            <div className=" flex flex-row items-center space-x-5 mt-6">
+              <div className="m-auto  grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 ">
+                {productData.map((item) => (
+                  <Product data={item} key={item._id} />
+                ))}
+              </div>
+            </div>
+          </div>
+      </main>
+      <HowItWorks/>
     </main>
   )
 }
